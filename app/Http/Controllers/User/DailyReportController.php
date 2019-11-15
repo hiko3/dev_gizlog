be801f91e71e6c4f99fd71c8fb7d988c6f595c79
金田　和彦 <>
2019-11-15 15:24:06 +0900

    daily_report/create.blade.phpの記述をフォームファサードに変更

--- app/Http/Controllers/User/DailyReportController.php
+++ app/Http/Controllers/User/DailyReportController.php
@@ -43,7 +43,7 @@ class DailyReportController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -4,20 +4,22 @@
 <h2 class="brand-header">日報作成</h2>
 <div class="main-wrap">
   <div class="container">
-      <input class="form-control" name="user_id" type="hidden">
-      <div class="form-group form-size-small">
-    <input class="form-control" name="reporting_time" type="date">
-    <span class="help-block"></span>
-    </div>
-    <div class="form-group">
-      <input class="form-control" placeholder="Title" name="title" type="text">
+    {!! Form::open(['route' => 'report.store']) !!}
+      {!! Form::input('hidden', 'user_id', NULL, ['class' => 'form-control']) !!}
+    <div class="form-group form-size-small">
+      {!! Form::input('date', 'reporting_time', NULL, ['class' => 'form-control']) !!}
       <span class="help-block"></span>
     </div>
     <div class="form-group">
-      <textarea class="form-control" placeholder="Content" name="contents" cols="50" rows="10"></textarea>
+      {!! Form::input('text', 'title', NULL, ['class' => 'form-control', 'placeholder' => 'Title']) !!}
       <span class="help-block"></span>
     </div>
-    <button type="submit" class="btn btn-success pull-right">Add</button>
+    <div class="form-group">
+      {!! Form::textarea('contents', NULL, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
+      <span class="help-block"></span>
+    </div>
+      {!! Form::button('Add', ['class' => 'btn btn-success pull-right']) !!}
+    {!! Form::close() !!}
   </div>
 </div>
 
