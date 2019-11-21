a1764bfaf6e9330ad67ba000fe748a4f8a84951d
金田　和彦 <>
2019-11-21 19:21:29 +0900

    ページネーションで検索結果保持に変更

--- resources/views/user/daily_report/index.blade.php
+++ resources/views/user/daily_report/index.blade.php
@@ -5,7 +5,7 @@
 <div class="main-wrap">
   <div class="btn-wrapper daily-report">
     {!! Form::open(['route' => ['report.index'], 'method' => 'GET']) !!}
-      {!! Form::input('month', 'search-month', $searchMonth , ['class' => 'form-control']) !!}
+      {!! Form::input('month', 'search-month', $searchMonth, ['class' => 'form-control']) !!}
       {!! Form::button('<i class="fa fa-search"></i>', ['class' => 'btn btn-icon', 'type' => 'submit']) !!}
     {!! Form::close() !!}
     <a class="btn btn-icon" href=" {{ route('report.create') }} "><i class="fa fa-plus"></i></a>
@@ -24,15 +24,15 @@
         @foreach ($reports as $report)
           <tr class="row">
             <td class="col-xs-2">{{ $report->reporting_time->format('m/d (D)') }}</td>
-            <td class="col-xs-3">{{ $report->title }}</td>
-            <td class="col-xs-5">{{ $report->content }}</td>
+            <td class="col-xs-3">{{ str_limit($report->title, 20) }}</td>
+            <td class="col-xs-5">{{ str_limit($report->content, 60) }}</td>
             <td class="col-xs-2"><a class="btn" href="{{ route('report.show', $report->id ) }}"><i class="fa fa-book"></i></a></td>
           </tr>
          @endforeach 
       </tbody>
     </table>
       <div class="text-center">
-        {{ $reports->links() }}
+        {{ $reports->appends(request()->input())->links() }}
       </div>
   </div>
 </div>
